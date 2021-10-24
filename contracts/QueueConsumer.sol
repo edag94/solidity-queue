be7ea093129ec55e2d154ae8b92a100301ad1c27
edag94 <edag94@live.com>
2021-10-23 21:11:10 -0400

    queue initialize

--- contracts/QueueConsumer.sol
+++ contracts/QueueConsumer.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.0;
 import "./Queue.sol";
 
 contract QueueConsumer {
-    using Queue for QueueStorage;
+    using Queue for Queue.QueueStorage;
 
     QueueStorage public queue;
+
+    constructor() {
+        queue.initialize();
+    }
 }
